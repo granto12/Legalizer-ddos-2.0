dd142f06e443010a935c08e259cdf3dc1938d640
granto12 <131477073+granto12@users.noreply.github.com>
2025-07-28 20:46:58 +0200

    Update playwright.js

--- playwright.js
+++ playwright.js
@@ -125,7 +125,7 @@ function getRandomUAConfig() {
   const configs = [
     {
       name: 'Windows Chrome',
-      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+      userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
       viewport: { width: 1920, height: 1080 },
       deviceScaleFactor: 1,
       isMobile: false,
